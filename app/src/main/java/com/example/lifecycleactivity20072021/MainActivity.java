b851daa022006e2b1d802f136c473fad971b295d
phamtanphat <phatdroid94@gmail.com>
2021-09-21 18:27:50 +0700

    saveinstance

--- app/src/main/java/com/example/lifecycleactivity20072021/MainActivity.java
+++ app/src/main/java/com/example/lifecycleactivity20072021/MainActivity.java
@@ -1,19 +1,41 @@
 package com.example.lifecycleactivity20072021;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     final String TAG = "MainActivity";
+    Button mBtnIntent;
+    String data = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"onCreate");
+
+        if (savedInstanceState != null){
+            data = savedInstanceState.getString("Data");
+            Log.d(TAG,data);
+        }
+
+        mBtnIntent = findViewById(R.id.buttonNavigateMain2);
+
+        mBtnIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent xác định hành cụ thể : explicit
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -50,5 +72,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("Data","abc");
     }
 }
