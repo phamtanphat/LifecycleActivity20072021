c310d6cd25d85b381a135987fa336848763e3772
phamtanphat <phatdroid94@gmail.com>
2021-09-21 19:08:56 +0700

    singletop

--- app/src/main/java/com/example/lifecycleactivity20072021/MainActivity2.java
+++ app/src/main/java/com/example/lifecycleactivity20072021/MainActivity2.java
@@ -2,18 +2,31 @@ package com.example.lifecycleactivity20072021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
     final String TAG = "MainActivity2";
+    Button mBtnIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Log.d(TAG,"onCreate");
+
+        mBtnIntent = findViewById(R.id.buttonNavigateMain3);
+        mBtnIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity3.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/lifecycleactivity20072021/MainActivity3.java
+++ app/src/main/java/com/example/lifecycleactivity20072021/MainActivity3.java
@@ -2,13 +2,78 @@ package com.example.lifecycleactivity20072021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    final String TAG = "MainActivity3";
+    Button mBtnIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Log.d(TAG,"onCreate");
+
+        mBtnIntent = findViewById(R.id.buttonNavigate);
+
+        mBtnIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity3.this,MainActivity3.class);
+                intent.putExtra("string","abc");
+                startActivity(intent);
+            }
+        });
+//        1 -> 2 -> 3 -> 2
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent != null){
+            String text = intent.getStringExtra("string");
+            Toast.makeText(MainActivity3.this, text, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
     }
 }
